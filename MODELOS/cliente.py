d80db55b6e72b36f30140aabb9e4344edb1ed236
https://github.com/CESAR-MENDOZA238/VENTAS_SERVICIO.git <cesar.mendozam@istpargentina.edu.pe>
2026-07-10 14:34:47 -0500

    inicio de proyecto

---
+++ MODELOS/cliente.py
@@ -0,0 +1,12 @@
+class Cliente:
+    def __init__(self, nombre, telefono, email, direccion, fechaRegistro):
+        self.id             =None
+        self.nombre         =nombre
+        self.telefono       =telefono
+        self.email          =email
+        self.direccion      =direccion
+        self.fechaRegistro  =fechaRegistro
+        
+        
+        
+        pass
