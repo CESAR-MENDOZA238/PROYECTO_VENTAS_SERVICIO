f4ed91b7871c20f172d87531f700436b670509cf
https://github.com/CESAR-MENDOZA238/VENTAS_SERVICIO.git <cesar.mendozam@istpargentina.edu.pe>
2026-08-10 02:49:02 -0500

    cambio de contraseña de base_datos

--- config/base_datos.py
+++ config/base_datos.py
@@ -14,7 +14,7 @@ def obtener_conexion():
     user = os.getenv("BD_USER", "postgres")
 
     # IMPORTANTE: Coloca tu contraseña real en el archivo .env
-    password = os.getenv("BD_PASSWORD", "123456")
+    password = os.getenv("BD_PASSWORD", "135421")
 
     # Nombre de la base de datos creada a partir de BD_VENTAS_SERVICIO.sql
     database = os.getenv("BD_NAME", "bd_ventas_servicio")
